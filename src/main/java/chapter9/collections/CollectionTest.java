23c3de37f0ecc2690540748c0ebfb1337fbc8f53
Opick <MTaufik@Outlook.Com>
2023-02-03 07:31:23 +0700

    Linked List

--- src/main/java/chapter9/collections/CollectionTest.java
+++ src/main/java/chapter9/collections/CollectionTest.java
@@ -1,11 +1,9 @@
 package chapter9.collections;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 public class CollectionTest {
   public static void main(String[] args) {
-//    Queue<String> stringLane = new CircularArrayQueue<>(100);
     Queue<String> stringLane = new ArrayDeque<>(100);
     stringLane.add("Asep");
     stringLane.add("Budi");
@@ -16,5 +14,23 @@ public class CollectionTest {
     for (String q : stringLane) {
       System.out.println(q);
     }
+
+    // Linked List
+    var staff = new LinkedList<String>();
+    staff.add("Amy");
+    staff.add("Bobby");
+    staff.add("Carl");
+
+    Iterator iter = staff.iterator();
+    String first = (String) iter.next();   // Visit first element
+    String second = (String) iter.next();  // Visit second element
+    iter.remove();  // Remove last visited element
+    System.out.printf("first : %s, second : %s.\n", first, second);
+
+    ListIterator listIter = staff.listIterator();
+    listIter.add("Dudung");
+    listIter.add("Encep");
+    for (String s : staff)
+      System.out.println(s);
   }
 }
